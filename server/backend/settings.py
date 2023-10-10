c14df3328a4029d7f7c0ab9f4298f4d9b954cbd7
leovrodriguez <lvr8@case.edu>
2023-10-10 16:33:34 -0400

    changed postgres, defined pip dependencies

--- server/backend/settings.py
+++ server/backend/settings.py
@@ -82,7 +82,7 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': env("DB_NAME"),
         'USER': env("DB_USER"),
         'PASSWORD': env("DB_PASSWORD"),
