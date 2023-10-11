65b72ba2ff75a8625c8a982f28810bf3173009f7
leovrodriguez <lvr8@case.edu>
2023-10-11 16:12:05 -0400

    edit instructions and configure for gcloud postgres instance

--- server/setupEnv.sh
+++ server/setupEnv.sh
@@ -7,14 +7,15 @@ python3 -m venv env
 env/bin/pip install -r dependencies.txt
 
 # create .env file in the backend directory with the provided arguments
-dbName=$1
+dbHost=$1
 dbUser=$2
-secretKey=$3
+dbPassword=$3
+dbName=$4
+secretKey=$5
 
 echo "DB_NAME=${dbName}" > backend/.env
-echo "DB_USER=postgres" >> backend/.env
-echo "DB_PASSWORD=${dbUser}" >> backend/.env
-echo "DB_HOST=127.0.0.1" >> backend/.env
-echo "DB_PORT=5432" >> backend/.env
-echo "DEBUG=on" >> backend/.env
+echo "DB_USER=${dbUser}" >> backend/.env
+echo "DB_PASSWORD=${dbPassword}" >> backend/.env
+echo "DB_HOST=${dbHost}" >> backend/.env
 echo "SECRET_KEY=${secretKey}" >> backend/.env
+echo "DEBUG=on" >> backend/.env
