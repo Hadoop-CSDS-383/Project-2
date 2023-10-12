18bb13dd0451df43f6027c78b935ca9cef59bf62
Jheanster <jctoledo935@gmail.com>
2023-10-12 10:04:43 -0400

    Can create events successfully
    Added full functionality of creating a event. Just need to figure out how to join the event on both the front end and the back end.

--- client/src/App.js
+++ client/src/App.js
@@ -1,162 +1,179 @@
 import './App.css';
-import { Calendar, dateFnsLocalizer } from 'react-big-calendar';
-import format from 'date-fns/format';
-import parse from 'date-fns/parse';
-import startOfWeek from 'date-fns/startOfWeek';
-import getDay from 'date-fns/getDay';
-import "react-big-calendar/lib/css/react-big-calendar.css";
-import React, { useEffect, useState } from 'react';
-import DatePicker from 'react-datepicker';
-import 'react-datepicker/dist/react-datepicker.css';
+import React, { useState, useEffect } from 'react';
+
+const events = [
+  {
+    title: "Big meeting",
+    desc: 'This is a big meeting',
+    email: 'email@email.com',
+    date: '2023-10-15',
+    time: '12:00 AM',
+    participants: [],
+  },
+  {
+    title: "Vacation",
+    desc: 'This is a vacation',
+    email: 'email@email.com',
+    date: '2023-10-19',
+    time: '10:00 AM',
+    participants: [],
+  },
+  {
+    title: "Conference",
+    desc: 'This is a conference',
+    email: 'email@email.com',
+    date: '2023-10-17',
+    time: '09:00 AM',
+    participants: [],
+  },
+];
+
+function App() {
+  const [newEvent, setNewEvent] = useState({ title: '', description: '', email: '', date: '', time: '' });
+  const [allEvents, setAllEvents] = useState(events);
+  const [showJoinScreen, setShowJoinScreen] = useState(false);
+  const [selectedEvent, setSelectedEvent] = useState(null);
+  const [participantData, setParticipantData] = useState({ email: '', name: '' });
+
+  function handleAddEvent() {
+    const newEventWithParticipants = { ...newEvent };
+    setAllEvents([...allEvents, newEventWithParticipants]);
+    setNewEvent({ title: '', description: '', email: '', date: '', time: '' });
+
+    // This should add to the database
+    fetch('api/events/create', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(newEventWithParticipants),
+    })
+      .then((response) => response.json())
+      .then((data) => console.log('Event added to the database:', data))
+      .catch((error) => console.error('Error adding event:', error));
+  }
 
 
+  useEffect(() => {
+    // Fetch events from the backend when the component mounts
+    fetch('api/events')
+      .then((response) => response.json())
+      .then((data) => setAllEvents(data))
+      .catch((error) => console.error('Error fetching events:', error));
+  }, []);
 
 
-const locales = { 
-  "en-US": require("date-fns/locale/en-US")
-}
+  function handleJoinEvent(event) {
+    setShowJoinScreen(true);
+    setSelectedEvent(event);
+  }
 
-const localizer = dateFnsLocalizer({
-  format,
-  parse,
-  startOfWeek,
-  getDay,
-  locales
-})
-
-
-// Be able to put a button on each event that says "Join Event"
-function CustomEvent({event}){
-  // Add the current users email to the list of participants.
-  const handleParticipants = () => {
-    event.participants.push("The email");
+  function handleJoin(event) {
+    const updatedEvents = allEvents.map((e) => {
+      if (e.title === event.title) {
+        return {
+          ...e,
+          participants: [...e.participants, participantData],
+        };
+      }
+      return e;
+    });
+    setAllEvents(updatedEvents);
+    setShowJoinScreen(false);
   }
 
   return (
-    <div className='custom-event'>
-      <div>{event.title}</div>
-
-      {/* I need to change this code to put the users email when they click join event*/}
-      <div>Participants: </div>
-      <button onClick={handleParticipants}>Join Event</button>
-    </div>
-  )
-}
-
-
-
-
-function App() {
-  const [ events, setEvents ] = useState([])
-
-  // Every time you make a new event, change these values.
-  const [ newEvent, setNewEvent ] = useState({ 
-    title: "",
-    start: "",
-    end: "",
-    description: "",
-    email: "",
-  })
-
-     useEffect(() => {
-      const fetchEvents = async () => {
-      const response = await fetch ('/api/events/')
-      const json = await response.json()
-  
-        if(response.ok){
-          console.log(json)
-          setEvents(json)
-  
-        }
-      }
-      fetchEvents()
-    },[])
-
-    const fetchEvents = async () => {
-      const response = await fetch('/api/events/');
-      const json = await response.json();
-  
-      if (response.ok) {
-        console.log(json);
-        setEvents(json);
-      }
-    };
-
-
-    // Add a new event both to the client and the database
-    function handleAddEvent() {
-      // Should add the event to the database here
-      fetch('api/events/create', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(newEvent),
-      })
-        .then((response) => {
-          if (response.ok) {
-
-            // This line should add the new event locally
-            setEvents([...events,newEvent]);
-
-            // This in theory adds it to the database
-            fetchEvents();
-            setNewEvent({
-              title: '',
-              start: '',
-              end: '',
-              description: '',
-            });
-          } else {
-            throw new Error('Failed to add the event to the database.');
-          }
-        })
-        .catch((error) => {
-          console.error(error);
-        });
-    }
-    
-  return (
     <div className='App'>
-      <h1>
-        Calendar
-      </h1>
-      <h2>Add New Event</h2>
+      <h1>Hadoop Project</h1>
       <div>
-        <input type="text" placeholder='Add Title 255 characters' style={{ width: "20%", marginRight: "10px"}}
-          value={newEvent.title} onChange={(e) => setNewEvent({...newEvent, title: e.target.value})} required maxLength={255}
-        />
-        <input type="email" placeholder='Email' style={{ width: "20%", marginRight: "10px"}}
-          value={newEvent.email} onChange={(e) => setNewEvent({...newEvent, email: e.target.value})} required maxLength={255}
-        />
-        <input type="text" placeholder='Add description 600 characters' style={{ width: "15%", height: "75px", marginRight: "10px", justifyContent:"center", alignItems:"flex-start"}}
-          value={newEvent.description} onChange={(e) => setNewEvent({...newEvent, description: e.target.value})} required maxLength={600}
-        />
 
-        <input type="text" placeholder='Time' style={{ width: "20%", marginRight: "10px", justifyContent:"center", alignItems:"flex-start"}}
-          value={newEvent.time} onChange={(e) => setNewEvent({...newEvent, time: e.target.value})} required maxLength={600}
+      <label for='eventTitle'> Event Title </label>
+        <input
+          type='text'
+          placeholder='255 characters'
+          id='eventTitle'
+          maxLength={255}
+          value={newEvent.title}
+          onChange={(e) => setNewEvent({...newEvent,title: e.target.value})}
         />
-        </div>
+        <label for='eventDesc'> Event Description </label>
+        <input
+          type='text'
+          placeholder='600 characters'
+          id='eventDesc'
+          maxLength={600}
+          value={newEvent.description}
+          onChange={(e) => setNewEvent({...newEvent,description: e.target.value})}
+        />
+        <label for='hostEmail'> Host email </label>
+        <input
+          type='email'
+          placeholder='Must be a valid email format'
+          id='hostEmail'
+          value={newEvent.email}
+          onChange={(e) => setNewEvent({...newEvent,email: e.target.value})}
+        />
+          <label for='date'> Event Date </label>
+          <input
+            type='text'
+            pattern='\d{4}-\d{2}-\d{2}'
+            placeholder='YYYY-MM-DD'
+            value={newEvent.date}
+            onChange={(e) => setNewEvent({...newEvent,date: e.target.value})}
+          />
+          <label for='time'> Event Time </label>
+          <input
+            type='text'
+            pattern='(0[1-9]|1[0-2]):[0-5][0-9] (AM|PM)'
+            placeholder='HH:MM AM/PM'
+            value={newEvent.time}
+            onChange={(e) => setNewEvent({...newEvent,time: e.target.value})}
+            />
+        <button style={{marginLeft: '10px'}} onClick={handleAddEvent}> Add Event </button>
+        
         <div>
-        <DatePicker placeholderText="Start Date" style={{marginRight: "10px"}}
-        selected={newEvent.start} onChange={(start) => setNewEvent({...newEvent,start})}/>
-        <DatePicker placeholderText="End Date"
-        selected={newEvent.end} onChange={(end) => setNewEvent({...newEvent,end})}/>
-        <button style={{marginTop: "10px"}} onClick={handleAddEvent}>Add Event</button>
+          <h2>Added Events</h2>
+          <ul>
+            {allEvents.map((event, index) => (
+              <ul key={index} className='event-list-item'>
+                <strong>Title:</strong> {event.title}<br />
+                <strong>Description:</strong> {event.description}<br />
+                <strong>Host email:</strong> {event.email}<br />
+                <strong>Date:</strong> {event.date}<br />
+                <strong>Time:</strong> {event.time} <br />
+                {showJoinScreen ? (
+                  selectedEvent === event ? (
+                    <>
+                      <label htmlFor='participantName'>Your Name</label>
+                      <input
+                        type='text'
+                        id='participantName'
+                        placeholder='Your Name'
+                        value={participantData.name}
+                        onChange={(e) => setParticipantData({ ...participantData, name: e.target.value })}
+                      />
+                      <label htmlFor='participantEmail'>Your Email</label>
+                      <input
+                        type='email'
+                        id='participantEmail'
+                        placeholder='Your Email'
+                        value={participantData.email}
+                        onChange={(e) => setParticipantData({ ...participantData, email: e.target.value })}
+                      />
+                      <button onClick={() => handleJoin(event)}>Join</button>
+                    </>
+                  ) : null
+                ) : (
+                  <button onClick={() => handleJoinEvent(event)}>Join this event</button>
+                )}
+                
+              </ul>
+            ))}
+          </ul>
+        </div>
       </div>
-      <Calendar 
-      // This basically shows how the calendar looks. What events are shown, what localizer (region) to use etc.
-      key={10}
-      localizer={localizer} events={events} 
-      startAccessor="start" endAccessor="end"
-      style={{ height: 500, margin: "50px" }} 
-        components={{ event: CustomEvent}}
-      />
     </div>
   );
 }
-
-
-
 
 export default App;
--- server/api/views.py
+++ server/api/views.py
@@ -1,12 +1,9 @@
-from django.shortcuts import render
 from django.http import JsonResponse
-
 import json
-import datetime
-
+import re
+from datetime import date, time
 from .models import Event, EventParticipant
 
-# Create your views here.
 def get_events(request):
     response = list(Event.objects.values())
     return JsonResponse(response, safe=False)
@@ -14,32 +11,69 @@ def get_events(request):
 def create_event(request):
     if request.method != "POST":
         return JsonResponse({'status': 'not ok'})
+
     body = json.loads(request.body)
-    parsed_date = body['date'].split('-')
-    date = datetime.date(int(parsed_date[0]), int(parsed_date[1]), int(parsed_date[2]))
-    pm = 2
-    parsed_time = body['time'].split(" ")
-    pm = 2 if parsed_time[1] == "PM" else 1
-    parsed_time = parsed_time[0].split(":")
-    time = datetime.time(int(parsed_time[0])*pm, int(parsed_time[1]))
-    title = body['title']
-    description = body['description']
-    email = body['email']
-    event = Event(date=date, time=time, title=title, description=description, email = email)
+
+    if 'date' not in body or 'time' not in body:
+        return JsonResponse({'status': 'missing date or time'})
+
+    date_str = body['date']
+    time_str = body['time']
+
+    # Validate date format (YYYY-MM-DD)
+    date_match = re.match(r'^(\d{4})-(\d{2})-(\d{2})$', date_str)
+    if not date_match:
+        return JsonResponse({'status': 'invalid date format'})
+
+    year, month, day = map(int, date_match.groups())
+    date_obj = date(year, month, day)
+
+    # Parse and validate time format (HH:MM AM/PM)
+    parsed_time = parse_time(time_str)
+    if parsed_time is None:
+        return JsonResponse({'status': 'invalid time format'})
+
+    title = body.get('title', '')
+    description = body.get('description', '')
+    email = body.get('email', '')
+
+    event = Event(date=date_obj, time=parsed_time, title=title, description=description, email=email)
     event.save()
+
     response = event.__dict__
     del response['_state']
+
     return JsonResponse(response, safe=False)
+
+def parse_time(time_str):
+    time_pattern = r'(\d{2}):(\d{2}) (AM|PM)'
+    match = re.match(time_pattern, time_str)
+
+    if match:
+        hours, minutes, period = match.groups()
+        hours = int(hours)
+        minutes = int(minutes)
+
+        if period == 'PM' and hours < 12:
+            hours += 12
+
+        return time(hours, minutes)
+
+    return None  # Return None for an invalid time format
 
 def subscribe_to_event(request, event):
     if request.method != "POST":
         return JsonResponse({'status': 'not ok'})
+
     body = json.loads(request.body)
-    event = Event.objects.get(pk=event)
+    event_obj = Event.objects.get(pk=event)
     name = body['name']
     email = body['email']
-    participant = EventParticipant(event=event, name=name, email=email)
+
+    participant = EventParticipant(event=event_obj, name=name, email=email)
     participant.save()
+
     response = participant.__dict__
     del response['_state']
+
     return JsonResponse(response, safe=False)
